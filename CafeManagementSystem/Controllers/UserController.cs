88a2d329c92850f9fb2df6d025e1d0aab2a5098f
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2024-12-23 00:22:09 +0100

    Added Login endpoint, corrected typos

--- CafeManagementSystem/Controllers/UserController.cs
+++ CafeManagementSystem/Controllers/UserController.cs
@@ -10,28 +10,57 @@ namespace CafeManagementSystem.Controllers
     [RoutePrefix("api/user")]
     public class UserController : ApiController
     {
-        CafeEntities db = new CafeEntities();
+        CafeEntities _db = new CafeEntities();
 
         [HttpPost, Route("signup")]
         public HttpResponseMessage Signup([FromBody] User user)
         {
             try
             {
-                User userObj = db.Users
+                User userObj = _db.Users
                     .Where(u => u.email == user.email).FirstOrDefault();
                 if (userObj == null)
                 {
                     user.role = "user";
                     user.status = "false";
-                    db.Users.Add(user);
-                    db.SaveChanges();
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
 
-                    return Request.CreateResponse(HttpStatusCode.OK, new { message = "Succesfully registred." });
-
+                    return Request.CreateResponse(HttpStatusCode.OK, new { message = "Successfully registered." });
                 }
                 else
                 {
                     return Request.CreateResponse(HttpStatusCode.BadRequest, new { message = "Email already exists." });
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost, Route("login")]
+        public HttpResponseMessage Login([FromBody] User user)
+        {
+            try
+            {
+                User userObj = _db.Users
+                    .Where(u => (u.email == user.email && u.password == user.password)).FirstOrDefault();
+                if (userObj != null)
+                {
+                    if (userObj.status == "true")
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            new { token = TokenManager.GenerateToken(userObj.email, userObj.role) });
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Wait for Admin approval" });
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized, new { message = "Incorrect Username or Password" });
                 }
             }
             catch (Exception e)
