59d351f1a6995447f14f86914c69504461900f0a
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2024-12-23 01:45:18 +0100

    Added GetPrincipal, ValidateToken methods to manage users, their session lifetime and validating their data.

--- CafeManagementSystem/TokenManager.cs
+++ CafeManagementSystem/TokenManager.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Web;
+using CafeManagementSystem.Models;
 
 namespace CafeManagementSystem
 {
@@ -27,6 +25,63 @@ namespace CafeManagementSystem
             JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
 
             return handler.WriteToken(token);
+        }
+
+        // validate received token and return user identity
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token); // parsing read token
+                if (jwtToken == null)
+                    return null;
+
+                // setting parameters - expiration time, sender and receiver validation if off, verify token with Secret
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret))
+                };
+
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+                return principal; // returning ClaimsPrincipal object representing user
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        // claiming email and role from token, cutting of "Bearer".
+        public static TokenClaim ValidateToken(string rawToken)
+        {
+            string[] array = rawToken.Split(' '); // splitting to parts, to separate "Bearer" prefix [0]
+            var token = array[1]; // Bearer is at [0], so we're getting rid of it
+            ClaimsPrincipal principal = GetPrincipal(token);
+            if (principal == null)
+                return null;
+
+            ClaimsIdentity identity = null;
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identity; // taking identity form principal object
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            // creating "TokenClaim" object that handle email and role for specific user
+            TokenClaim tokenClaim = new TokenClaim();
+            var temp = identity.FindFirst(ClaimTypes.Email);
+            tokenClaim.Email = temp.Value;
+            temp = identity.FindFirst(ClaimTypes.Role);
+            tokenClaim.Role = temp.Value;
+            return tokenClaim;
         }
     }
 }
