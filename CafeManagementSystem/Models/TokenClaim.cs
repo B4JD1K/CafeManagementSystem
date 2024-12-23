fe5ae3dc2f91101513326fac73c18f7495560555
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2024-12-23 01:10:26 +0100

    Created model of TokenClaim (for Email and Role)

---
+++ CafeManagementSystem/Models/TokenClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeManagementSystem.Models
+{
+    public class TokenClaim
+    {
+        public string Email { get; set; }
+        
+        public string Role { get; set; }
+    }
+}
